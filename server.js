2c28cb3be70b80c023d32c4a5190164661153e24
amilbcahat <amilbcahat@gmail.com>
2022-09-01 15:52:02 +0530

    make it better

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ mongoose
 
 const app = require("./app");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
 });
