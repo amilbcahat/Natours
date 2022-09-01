6eac3b9762b911bee9231cda641fceceb6e444bf
amilbcahat <amilbcahat@gmail.com>
2022-09-01 16:03:10 +0530

    make it better

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ process.on("uncaughtException", (err) => {
 });
 
 dotenv.config({ path: "./config.env" });
-const DB = process.env.DATABASE_LOCAL;
-// .replace(
-//   "<PASSWORD>",
-//   process.env.DATABASE_PASSWORD
-// );
+const DB = process.env.DATABASE;
 
 mongoose
   .connect(DB, {
