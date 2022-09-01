eb55e8f78f984ce7449c5be562b3fbbbb3d2a13c
amilbcahat <amilbcahat@gmail.com>
2022-09-01 21:31:11 +0530

    NOW ACTUALLY FIXED EVERYTHING

--- controllers/authController.js
+++ controllers/authController.js
@@ -46,7 +46,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   const url = `${req.protocol}://${req.get("host")}/me`;
   // console.log(url);
   // console.log(newUser);
-  await new Email(newUser, url).sendWelcome();
+  // await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res);
 });
