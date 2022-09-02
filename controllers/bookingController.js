7fc01db6eb458065468ec12056920b1e7fb4a508
amilbcahat <amilbcahat@gmail.com>
2022-09-02 16:26:29 +0530

    Again committed

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -64,7 +64,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = await User.findOne({ email: session.customer_email }).id;
-  const price = session.display_items[0].price_data.unit_amount;
+  const price = session.line_items[0].price_data.unit_amount;
   await Booking.create({ tour, user, price });
 };
 
